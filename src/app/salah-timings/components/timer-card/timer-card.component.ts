d7f546e17364ad69e5aca5d72ba04bee74863f1d
SabikAbtahee <sabikchamp@gmail.com>
2024-03-16 14:03:18 +0600

    chore: design finalized

--- src/app/salah-timings/components/timer-card/timer-card.component.ts
+++ src/app/salah-timings/components/timer-card/timer-card.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-timer-card',
   templateUrl: './timer-card.component.html',
-  styleUrl: './timer-card.component.scss'
+  styleUrl: './timer-card.component.scss',
 })
 export class TimerCardComponent {
+  @Input({ required: true })
+  text!: string;
 
+  timeText!: string;
+  timeNumber!: string;
+  @Input({ required: true }) set time(value: string | undefined) {
+    if (value) {
+      const values = value.split(" ");
+      this.timeNumber = values[0];
+      this.timeText = values[1];
+    }
+   };
 }
