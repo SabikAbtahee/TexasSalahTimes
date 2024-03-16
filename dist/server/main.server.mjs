0c20ad07b5bb7f1f422596c7609a716da37a6e67
SabikAbtahee <sabikchamp@gmail.com>
2024-03-16 16:37:50 +0600

    chore: alignment change

--- dist/server/main.server.mjs
+++ dist/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-HO7GNO74.mjs";import"./chunk-FT7R42PL.mjs";import"./chunk-FXA75CCY.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-OXZHSB2X.mjs";import"./chunk-FT7R42PL.mjs";import"./chunk-FXA75CCY.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
