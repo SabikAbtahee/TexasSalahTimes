c120e9bff45029a8dd4ac13dc925542fe28ebb01
SabikAbtahee <sabikchamp@gmail.com>
2024-04-01 22:09:36 +0600

    chore: node code

--- src/environments/environment.ts
+++
@@ -1,3 +0,0 @@
-export const environment = {
-  PrayerTimings:"https://us.mohid.co/tx/dallas/ialfm/masjid/widget/api/index/?m=prayertimings"
-}
--- src/main.server.ts
+++
@@ -1,1 +0,0 @@
-export { AppServerModule as default } from './app/root/app.module.server';
