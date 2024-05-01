17d3fa98fca2bc97ca5bd4ef562e943f3ab32f75
Jamal Ahmed <61325519+xamalahmed77@users.noreply.github.com>
2024-05-01 09:11:45 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   PrayerTimings: "https://us.mohid.co/tx/dallas/ialfm/masjid/widget/api/index/?m=prayertimings",
-  CurrentImageName:"New Comers.png"
+  CurrentImageName:"may1v2.png"
 }
