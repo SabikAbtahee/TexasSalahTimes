ea2623aeca65eba0a6456dd119ee2f3b63edcabc
Jamal Ahmed <jamal@rtp99.com>
2024-05-01 08:49:27 -0500

    may1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   PrayerTimings: "https://us.mohid.co/tx/dallas/ialfm/masjid/widget/api/index/?m=prayertimings",
-  CurrentImageName:"New Comers.png"
+  CurrentImageName:"may1.png"
 }
+//2024-04-19
