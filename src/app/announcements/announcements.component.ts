2b5145fa95df670a5fd7959a8e0ae4d3736143fd
Jamal Ahmed <jamal@rtp99.com>
2024-08-08 19:28:07 -0500

    5

--- src/app/announcements/announcements.component.ts
+++ src/app/announcements/announcements.component.ts
@@ -21,4 +21,4 @@ export class AnnouncementsComponent {
     this.currentAnnouncementImagePath = this.fileManager.getCurrentAnnouncementImage(environment.CurrentImageName);
   }
 }
-//update4
+//update5
