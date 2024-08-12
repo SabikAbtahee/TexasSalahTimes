b4fb8722e32c71c320bf9e018e0cb8558f4d34b9
Jamal Ahmed <jamal@rtp99.com>
2024-08-12 00:34:12 -0500

    Pilars Academy flyer (1920 x 720 px).png

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   PrayerTimings: "https://us.mohid.co/tx/dallas/ialfm/masjid/widget/api/index/?m=prayertimings",
-  CurrentImageName:"Pilars Academy flyer.jpg"
+  CurrentImageName:"Pilars Academy flyer (1920 x 720 px).png"
 }
